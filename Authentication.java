2fbdcc9837d82d094ea1edbcb8eea337ba0905cc
CS19B010Gowtami <70938268+CS19B010Gowtami@users.noreply.github.com>
2020-11-11 20:48:47 +0530

    Update Authentication.java

--- Authentication.java
+++ Authentication.java
@@ -5,22 +5,22 @@ class Authentication  extends GUI
 	boolean yn,n=false;
 	BankDataBase bdb=new BankDataBase();
 	keypad keypad1=new keypad();
-  public boolean validacno(int acno)
-	{
-      this.acno=acno;
-	    if(Digit.size(acno)==5)
-		  {
-			return true;
+       public boolean validacno(int acno)
+       {
+           this.acno=acno;
+	   if(Digit.size(acno)==5)
+	   {
+		return true;
 	    }
 	    return false;
 	}
-    public boolean validpinno(int acno,int pinno)
-    {
-    	yn=bdb.issearchpinno(acno,pinno);
-		  if(yn==true)
-		  {
-			 return true;
-		  }
-	    return false;
-    }
+       public boolean validpinno(int acno,int pinno)
+       {
+    	 yn=bdb.issearchpinno(acno,pinno);
+	 if(yn==true)
+	  {
+              return true;
+	  }
+	   return false;
+      }
 }
