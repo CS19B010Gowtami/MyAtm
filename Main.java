a457ac82fc0c8fd5cd0f8072fdfa05af009af49f
CS19B010Gowtami <70938268+CS19B010Gowtami@users.noreply.github.com>
2020-11-11 20:56:25 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,28 +4,28 @@ import java.util.*;
 import javax.swing.*;
 public class MyAtm
 {
-	    public static void main(String[] args)
+     public static void main(String[] args)
       {
     	   GUI gui=new GUI();
-		     gui.LoginGUI();
+	    gui.LoginGUI();
        }
        public static void setacno(int acno1)
-    	 {
-    		   int acno=acno1;
-           MyMenu obj=new MyMenu();//object of MyClass
-    	     int yn = 0;
+        {
+    	   int acno=acno1;
+            MyMenu obj=new MyMenu();//object of MyClass
+    	    int yn = 0;
     	     keypad keypad1=new keypad();
-    	      outerprinter.welcome();
+    	     outerprinter.welcome();
     	     do 
-    		    {
+    	     {
     	         obj.myMenu(acno);
-    			     outerprinter.DisplayMessageln("do you want to exit:");
-    			     outerprinter.DisplayMessageln("Choose 1(for yes) or 0(for no)");
-    			     yn = keypad1.getInput();
+    	         outerprinter.DisplayMessageln("do you want to exit:");
+    	         outerprinter.DisplayMessageln("Choose 1(for yes) or 0(for no)");
+    	          yn = keypad1.getInput();
     	      }
-    		     while (yn==0);
-    	      if(yn==1)
-    	      {
+    	     while (yn==0);
+    	     if(yn==1)
+    	     {
     	        outerprinter.end();
     	      }
     	 }
