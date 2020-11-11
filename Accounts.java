8900d68a6921dbae04825dd04f32bbf2ddf7a631
CS19B010Gowtami <70938268+CS19B010Gowtami@users.noreply.github.com>
2020-11-11 20:18:36 +0530

    Create Accounts.java

---
+++ Accounts.java
@@ -0,0 +1,39 @@
+class Accounts
+{
+	private int acno;
+	private int pinno;
+	private int balance;
+	private long phno;
+	public void setacno(int acno)
+	{
+		this.acno=acno;
+	}
+	public void setpinno(int pinno)
+	{
+		this.pinno=pinno;
+	}
+	public void setbalance(int balance)
+	{
+		this.balance=balance;
+	}
+	public void setphoneno(long phno)
+	{
+		this.phno=phno;
+	}
+    public int getacno()
+	{
+		return acno;
+	}
+	public int getpinno()
+	{
+		return pinno;
+	}
+	public int getbalance()
+	{
+		return balance;
+	}
+	public long getphoneno()
+	{
+		return phno;
+	}
+}
