773a50e7413866c5f975fa32b0f6eb79065ade7e
CS19B010Gowtami <70938268+CS19B010Gowtami@users.noreply.github.com>
2020-11-11 20:01:04 +0530

    Create Authentication.java

---
+++ Authentication.java
@@ -0,0 +1,26 @@
+class Authentication  extends GUI
+{
+	protected int acno;
+	protected int pinno,userpin;
+	boolean yn,n=false;
+	BankDataBase bdb=new BankDataBase();
+	keypad keypad1=new keypad();
+  public boolean validacno(int acno)
+	{
+      this.acno=acno;
+	    if(Digit.size(acno)==5)
+		  {
+			return true;
+	    }
+	    return false;
+	}
+    public boolean validpinno(int acno,int pinno)
+    {
+    	yn=bdb.issearchpinno(acno,pinno);
+		  if(yn==true)
+		  {
+			 return true;
+		  }
+	    return false;
+    }
+}
