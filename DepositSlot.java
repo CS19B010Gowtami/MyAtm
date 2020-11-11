b25d0e8df7a2e9f7616f8053076899f60e9ab1fd
CS19B010Gowtami <70938268+CS19B010Gowtami@users.noreply.github.com>
2020-11-11 20:51:22 +0530

    Update DepositSlot.java

--- DepositSlot.java
+++ DepositSlot.java
@@ -2,25 +2,25 @@ class DepositSlot extends ViewMyBalance
 {
 	public  void deposit(int acno)
 	{
-		String filepath="accounts.csv";
-		keypad keypad1=new keypad();
-		int balance=getBalance(acno);
-		outerprinter.DisplayMessageln("Enter the amount to deposit  or choose 0 for cancellation");
+	   String filepath="accounts.csv";
+           keypad keypad1=new keypad();
+           int balance=getBalance(acno);
+            outerprinter.DisplayMessageln("Enter the amount to deposit  or choose 0 for cancellation");
 	    int dp=keypad1.getInput();
 	    int newBalance=balance+dp;
 	    if(dp!=0)
-    	{
-    	   outerprinter.DisplayMessageln("Please send the envelop....");
-    	    outerprinter.DisplayMessage("your present balance is:");
-    	    outerprinter.DisplayIntMessage(newBalance);
-    	    outerprinter.DisplayMessageln("(in rupees)");
-    	    String sacno=String.valueOf(acno);
-    		String snewBalance=String.valueOf(newBalance);
-    		Update.updateCSV(filepath, sacno, snewBalance);
-         }
-        else
-    	{
-    		outerprinter.DisplayMessagelninred("Cancelling your transaction.....");
-    	}
+    	    {
+    	       outerprinter.DisplayMessageln("Please send the envelop....");
+    	       outerprinter.DisplayMessage("your present balance is:");
+    	       outerprinter.DisplayIntMessage(newBalance);
+    	       outerprinter.DisplayMessageln("(in rupees)");
+    	       String sacno=String.valueOf(acno);
+    	       String snewBalance=String.valueOf(newBalance);
+    	       Update.updateCSV(filepath, sacno, snewBalance);
+           }
+          else
+    	  {
+    	    outerprinter.DisplayMessagelninred("Cancelling your transaction.....");
+    	  }
 	}
 }
