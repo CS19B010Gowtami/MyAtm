caa698c6b64d19ca9e7389100679c39ee93fa015
CS19B010Gowtami <70938268+CS19B010Gowtami@users.noreply.github.com>
2020-11-11 20:09:40 +0530

    Create outerprinter.java

---
+++ outerprinter.java
@@ -0,0 +1,36 @@
+class outerprinter
+{
+	 public static void DisplayIntMessageinred(int message)
+		{
+			System.err.print(message);
+		}
+    public static void DisplayMessageln(String message)
+	 {
+		System.out.println(message);
+	}
+   public static void DisplayMessagelninred(String message)
+	{
+		System.err.println(message);
+	}
+	public static void DisplayMessage(String message)
+	{
+		System.out.print(message);
+	}
+   public static void DisplayIntMessage(int message)
+	{
+		System.out.print(message);
+	}
+    public static void welcome()
+	{
+		System.out.println("Welcome to our Bank");
+	}
+	public static void end()
+	{
+		System.out.println("Thanks for using our Bank!!");
+		System.out.println("Sorry for any inconvineance from our side");
+	}
+	public void displayamount(double amount)
+	{
+		System.out.println(amount +"rs");
+	}
+}
